9eebe7f1b470f4dfcd031d0e8c55c54aff075663
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-04-04 12:47:05 +0100

    Dev

---
+++ lowkey/src/components/navBar.jsx
@@ -0,0 +1,20 @@
+import "./css/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <ul className="navBarList">
+        <li>
+          <h1>Lowkey</h1>
+        </li>
+        <li>
+          <input></input>
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
