9fbb7e7b0a96258a01a62d4aadeba1f16dbb00b8
david <dmneto2005@gmail.com>
2024-04-10 12:55:10 +0100

    base minimamente feita do jsx, falta mudar o css

--- lowkey/src/components/login-page/LoginPage.jsx
+++ lowkey/src/components/login-page/LoginPage.jsx
@@ -36,30 +36,30 @@ import "./LoginPage.css";
 }; */
 
 const LoginPage = () => {
-    return (
-      <div className="loginPage">
-        <div className="loginForm">
-          <h1>Login</h1>
-          <form className="loginForm">
-            <input
-              type="text"
-              placeholder="Username"
-              className="loginInput"
-            ></input>
-            <input
-              type="password"
-              placeholder="Password"
-              className="loginInput"
-            ></input>
-            <input
-              type="submit"
-              value="Login"
-              className="loginButton"
-            ></input>
-          </form>
-        </div>
+  return (
+    <div className="loginPage">
+      <div className="loginForm">
+        <h1>Login</h1>
+        <form className="loginForm">
+          <input
+            type="text"
+            placeholder="Username"
+            className="loginInput"
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            className="loginInput"
+          ></input>
+          <input
+            type="submit"
+            value="Login"
+            className="loginButton"
+          ></input>
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default LoginPage;
