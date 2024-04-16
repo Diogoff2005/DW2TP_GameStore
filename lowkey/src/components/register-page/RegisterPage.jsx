6812d514c6a61e5daf02a6edaf7fb841f98474ad
david <dmneto2005@gmail.com>
2024-04-16 16:45:09 +0100

    nao consigo meter as labels pra esquerda help

--- lowkey/src/components/register-page/RegisterPage.jsx
+++ lowkey/src/components/register-page/RegisterPage.jsx
@@ -1,35 +1,37 @@
 import Buttonsubmit from "../Buttonsubmit/Buttonsubmit";
 import "./RegisterPage.css";
 
-
-
 const RegisterPage = () => {
   return (
     <div className="RegisterPage">
       <div className="RegisterForm">
         <h2>Register</h2>
         <form className="RegisterForm">
+          <label>Username:</label>
           <input
             type="text"
             placeholder="Username"
             className="RegisterinInput"
           ></input>
+          <label>Password:</label>
           <input
             type="password"
             placeholder="Password"
             className="RegisterinInput"
           ></input>
+          <label>Repeat Password:</label>
           <input
             type="password"
             placeholder="Repeat Password"
             className="RegisterinInput"
           ></input>
+          <label>E-mail:</label>
           <input
             type="text"
             placeholder="E-mail"
             className="RegisterinInput"
           ></input>
-          <Buttonsubmit textobutton={"Register"}/>
+          <Buttonsubmit textobutton={"Register"} />
         </form>
       </div>
     </div>
