0eb410bcbd7abd19c3e84bf108b06ed1b3b7625c
Dinis Ferreira <dinis0707@gmail.com>
2024-04-17 09:00:03 +0000

    Card V1

--- lowkey/src/App.js
+++ lowkey/src/App.js
@@ -1,19 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navBar/NavBar";
+import Card from "./components/card/card";
 
 function App() {
   return (
-    <header
-      style={{
-        backgroundColor: "black",
-        width: "100%",
-        borderBottom: "5px solid red",
-      }}
-      className="App-header"
-    >
-      <NavBar />
-    </header>
+    <>
+      <header
+        style={{
+          backgroundColor: "black",
+          width: "100%",
+          borderBottom: "5px solid red",
+        }}
+        className="App-header"
+      >
+        <NavBar />
+      </header>
+      <Card />
+    </>
   );
 }
 
