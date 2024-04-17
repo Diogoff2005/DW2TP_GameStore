a64737b3098cb152418bfc889fcb2e9815c35cf8
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-04-17 11:03:19 +0100

    Adição de alguns componentes

--- lowkey/src/paginas/home.jsx
+++ lowkey/src/paginas/home.jsx
@@ -1,3 +1,23 @@
-const Home = () => {};
+import NavBar from "../components/navBar/NavBar";
+import Banner from "../components/Banner/Banner";
+import bannerImage from "../components/Banner/banner.jpg";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <main
+        style={{
+          margin: "0px",
+          padding: "0px",
+        }}
+      >
+        <Banner path={bannerImage}></Banner>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
