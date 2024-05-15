570613b180e48450c0960e41c15298580a143e3d
Gabriel <gabifonte37@gmail.com>
2024-05-15 12:19:09 +0100

    fix h3

--- lowkey/src/components/CartCounter/CartCounter.jsx
+++ lowkey/src/components/CartCounter/CartCounter.jsx
@@ -5,9 +5,9 @@ import Buttonsubmit from "../Buttonsubmit/Buttonsubmit";
 const CartCounter = ({ offPrice, discounts }) => {
   return (
     <div>
-      <h3> Official price {offPrice}$ </h3>
-      <h3> Discounts {-discounts}$ </h3>
-      <h3>Total {offPrice-discounts }$</h3>
+      <h3 className="h3"> Official price {offPrice}$ </h3>
+      <h3 className="h3"> Discounts {-discounts}$ </h3>
+      <h3 className="h3">Total {offPrice-discounts }$</h3>
       <div className="align">
       <Buttonsubmit
         className={"Payment"}
