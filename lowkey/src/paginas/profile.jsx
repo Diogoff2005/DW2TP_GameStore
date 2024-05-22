65691d7e9878497c2ea4643ceea5d95083d9bcd1
Dinis Ferreira <dinis0707@gmail.com>
2024-05-22 11:21:09 +0000

    profile page feita

--- lowkey/src/paginas/profile.jsx
+++ lowkey/src/paginas/profile.jsx
@@ -1,6 +1,8 @@
 import ProfileDetails from "../components/profileDetails/ProfileDetails";
 import ProfileStats from "../components/profileStats/ProfileStats";
+import GameWithDetails from "../components/GameWithDetails/GameWithDetails";
 import iCon from "../components/profileDetails/icon.png";
+import "./profile.css";
 
 const pFP = "https://i.redd.it/q8o37kcrenya1.jpg";
 const uSername = "Dinis Ferreira";
@@ -17,15 +19,40 @@ const Profile = () => {
           padding: "0px",
         }}
       >
-        <ProfileDetails
-          PFP={pFP}
-          username={uSername}
-          email={eMail}
-          creationDate={cReationDate}
-          icon={iCon}
-        />
+        <div className="profile">
+          <ProfileDetails
+            PFP={pFP}
+            username={uSername}
+            email={eMail}
+            creationDate={cReationDate}
+            icon={iCon}
+          />
 
-        <ProfileStats saved={1} avgSave={12} highSave={12} />
+          <h2 className="inText">Jogos comprados</h2>
+
+          <GameWithDetails
+            name="God of War"
+            picture={pFP}
+            price="yes"
+            developer="yes"
+            publisher="yes"
+            genre="yes"
+            releaseDate="disja"
+            gameKey="2134567890"
+          />
+          <GameWithDetails
+            name="God of War"
+            picture={pFP}
+            price="yes"
+            developer="yes"
+            publisher="yes"
+            genre="yes"
+            releaseDate="disja"
+            gameKey="2134567890"
+          />
+
+          <ProfileStats saved={1} avgSave={12} highSave={12} />
+        </div>
       </main>
     </>
   );
