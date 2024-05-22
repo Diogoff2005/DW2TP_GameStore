74966f076442e379c43296ab6d96decdfc6084ea
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-05-22 10:48:46 +0100

    Card Id

--- lowkey/src/components/card/card.jsx
+++ lowkey/src/components/card/card.jsx
@@ -1,19 +1,23 @@
 import imagem from "./GOW.jpg";
 import "./Card.css";
-const Card = ({ name, price, imagem }) => {
+import { Link } from "react-router-dom";
+
+const Card = ({ id, name, price, imagem }) => {
   return (
-    <div
-      className="card grid-container"
-      style={{
-        background: `url(${imagem})`,
-        backgroundSize: `cover`,
-        minWidth: `460px`,
-        maxWidth: `460px`,
-      }}
-    >
-      <h2 className="GameName col-4">{name}</h2>
-      <h2 className="price col-2">{price}€</h2>
-    </div>
+    <Link to={`/game/${id}`}>
+      <div
+        className="card grid-container"
+        style={{
+          background: `url(${imagem})`,
+          backgroundSize: `cover`,
+          minWidth: `460px`,
+          maxWidth: `460px`,
+        }}
+      >
+        <h2 className="GameName col-4">{name}</h2>
+        <h2 className="price col-2">{price}€</h2>
+      </div>
+    </Link>
   );
 };
 
